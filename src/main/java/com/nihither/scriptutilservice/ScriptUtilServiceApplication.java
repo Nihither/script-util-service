8289b596b96a48a859bdbcdce2bf078e3ee1cb75
Andrew Semenov <semenov.an.sh@gmail.com>
2024-05-08 21:23:26 +0300

    initial project

---
+++ src/main/java/com/nihither/scriptutilservice/ScriptUtilServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nihither.scriptutilservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptUtilServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptUtilServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nihither/scriptutilservice/ScriptUtilServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nihither.scriptutilservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptUtilServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
