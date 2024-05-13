b7383a121d3ad1710584179d988c9d19fb48a286
Andrew Semenov <semenov.an.sh@gmail.com>
2024-05-13 22:03:47 +0300

    add appRunning test

--- src/test/java/com/nihither/scriptutilservice/ScriptUtilServiceApplicationTests.java
+++ src/test/java/com/nihither/scriptutilservice/ScriptUtilServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.nihither.scriptutilservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ScriptUtilServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void appIsRunning() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isUnauthorized());
 	}
 
 }
