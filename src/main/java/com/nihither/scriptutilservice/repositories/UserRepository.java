55c708c30d11b3c7efaab9ed3b497bd6011ee1e3
Andrew Semenov <semenov.an.sh@gmail.com>
2024-05-13 22:03:26 +0300

    fix code mistake

--- src/main/java/com/nihither/scriptutilservice/repositories/UserRepository.java
+++ src/main/java/com/nihither/scriptutilservice/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.nihither.scriptutilservice.repositories;
 
 import com.nihither.scriptutilservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
