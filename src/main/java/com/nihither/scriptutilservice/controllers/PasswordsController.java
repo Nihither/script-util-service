943040411f0e0a852d8a3cd36a3deb7d40b3b3a8
Andrew Semenov <semenov.an.sh@gmail.com>
2024-05-13 22:04:20 +0300

    add passRepository, find save methods

---
+++ src/main/java/com/nihither/scriptutilservice/controllers/PasswordsController.java
@@ -0,0 +1,65 @@
+package com.nihither.scriptutilservice.controllers;
+
+import com.nihither.scriptutilservice.models.Password;
+import com.nihither.scriptutilservice.repositories.PasswordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api")
+public class PasswordsController {
+
+    @Autowired
+    private PasswordRepository passwordRepository;
+
+    @GetMapping("/passwords/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<?> getPasswordById(@PathVariable("id") Long id) {
+        Optional<Password> password = passwordRepository.findById(id);
+
+        if (password.isPresent()) {
+            return new ResponseEntity<>(password, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Password with id=" + id + " not found", HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/passwords")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<?> getAllPasswordsByContaining(@RequestParam() String searchString) {
+        try {
+            List<Password> passwords = new ArrayList<Password>();
+            passwords.addAll(passwordRepository.findAllByContaining(searchString));
+            if (passwords.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(passwords, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(String.valueOf(e), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/passwords")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<?> createPassword(Password password) {
+        password.setCreation_time(new Date());
+        password.setLast_modification_time(new Date());
+        password.setRmv(false);
+        try {
+            passwordRepository.save(password);
+            return new ResponseEntity<>("Password successfully saved into DB", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(String.valueOf(e), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
